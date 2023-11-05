a8cf33ae297f1203acef5d66a52d115005c1d200
kazirauf <raufelahi9876@gmail.com>
2023-11-05 09:55:18 +0600

    The work of making the banner is over

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Page/Shared/Navbar/Navbar";
+import Footer from "../Page/Shared/Footer/Footer";
 
 const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Page/Home/Banner/Banner.jsx
+++ src/Page/Home/Banner/Banner.jsx
@@ -1,11 +1,27 @@
-
-
 const Banner = () => {
-    return (
-        <div>
+  return (
+    <div>
+      <div data-aos="fade-down"
+     data-aos-easing="linear"
+     data-aos-duration="1500" className="hero min-h-[800px]">
+        <div className="hero-content flex-col lg:flex-row-reverse gap-20">
+          <img
+            src="https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?auto=format&fit=crop&q=80&w=1744&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            className="max-w-2xl rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">
+              <span className="text-fuchsia-400">If you want</span> to get job then you have come to right place
+            </h1>
+            <p className="py-6">
+            If you are seeking employment opportunities, you have landed in the perfect spot. Our platform offers a wide range of job listings and resources to help you kickstart your career journey with confidence.
+            </p>
             
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
